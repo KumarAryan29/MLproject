3c166c3184131d150b9dd0cdc5084480b512778e
Aryan_Kumar <giet.aryan227@gmail.com>
2025-10-06 17:12:04 +0530

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -16,8 +16,8 @@ class CustomerException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
-def __str__(self):
-    return self.error_message
+    def __str__(self):
+        return self.error_message
 
 
 # For checking the code
